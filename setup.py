a539c5bfadfb1ba88f7b6302b84fe51dea62317a
novatechnolab <novatechnolab@gmail.com>
2026-03-20 19:02:15 +0530

    Version 1.0.0 - Release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="jupyter-ai-extension",
-    version="1.0.1",
+    version="1.0.0",
     author="Nova Technolab",
     # ADD THESE TWO LINES:
     license="MIT",
